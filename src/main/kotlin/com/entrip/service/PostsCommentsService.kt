8d55fcae3a950455fd802235cd970e30614e72fd
donghwan <wkazxf@gmail.com>
2022-08-10 20:21:10 +0900

    [CHORE] Remove Unused Import

--- src/main/kotlin/com/entrip/service/PostsCommentsService.kt
+++ src/main/kotlin/com/entrip/service/PostsCommentsService.kt
@@ -12,9 +12,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.util.*
 import javax.transaction.Transactional
-import kotlin.collections.ArrayList
 
 @Service
 class PostsCommentsService(
