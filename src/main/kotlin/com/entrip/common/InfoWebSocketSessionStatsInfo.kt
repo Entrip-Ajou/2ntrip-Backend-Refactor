e497f7d95b6e20be41d275dbabfb61655e2f302d
donghwan <wkazxf@gmail.com>
2022-08-12 16:10:45 +0900

    [FEAT] InfoWebSocketSessionStatsInfo

---
+++ src/main/kotlin/com/entrip/common/InfoWebSocketSessionStatsInfo.kt
@@ -0,0 +1,19 @@
+package com.entrip.common
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats
+
+class InfoWebSocketSessionStatsInfo(
+    @Autowired
+    private val webSocketMessageBrokerStats: WebSocketMessageBrokerStats
+) {
+    private val logger: Logger = LoggerFactory.getLogger(InfoWebSocketSessionStatsInfo::class.java)
+
+    @Scheduled(fixedDelay = 30000)
+    public fun infoWebSocketSessionStatsInfo() {
+        logger.info(webSocketMessageBrokerStats.webSocketSessionStatsInfo.toString())
+    }
+}
--- src/main/kotlin/com/entrip/common/WebSocketEventListener.kt
+++ src/main/kotlin/com/entrip/common/WebSocketEventListener.kt
@@ -4,15 +4,17 @@ import com.entrip.domain.MessageType
 import com.entrip.domain.SocketMessages
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.event.EventListener
 import org.springframework.messaging.simp.SimpMessageSendingOperations
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats
 import org.springframework.web.socket.messaging.SessionConnectedEvent
 import org.springframework.web.socket.messaging.SessionDisconnectEvent
 
 class WebSocketEventListener(
-    private val messagingTemplate: SimpMessageSendingOperations?,
-
+    private val messagingTemplate: SimpMessageSendingOperations?
     ) {
     private val logger: Logger = LoggerFactory.getLogger(WebSocketEventListener::class.java)
 
