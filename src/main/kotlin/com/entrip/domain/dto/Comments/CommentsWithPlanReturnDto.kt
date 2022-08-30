0679b629fe62d249075079f4fe1e073af5f47284
Egenieee <hhgg0925@naver.com>
2022-08-30 23:39:32 +0900

    [FIX] 반환 데이터 타입 불일치 수정 - 202코드 날릴 때도 CommentsWithPlanReturnDto 타입으로 반환하되, null로 값 채워 반환하도록 함

--- src/main/kotlin/com/entrip/domain/dto/Comments/CommentsWithPlanReturnDto.kt
+++ src/main/kotlin/com/entrip/domain/dto/Comments/CommentsWithPlanReturnDto.kt
@@ -2,9 +2,9 @@ package com.entrip.domain.dto.Comments
 
 import com.entrip.domain.dto.Plans.PlansReturnDto
 
-class CommentsWithPlanReturnDto (
-    val planReturnDto: PlansReturnDto,
-    val commentsList: MutableList<CommentsReturnDto> = ArrayList<CommentsReturnDto>()
+class CommentsWithPlanReturnDto(
+    val planReturnDto: PlansReturnDto?,
+    val commentsList: MutableList<CommentsReturnDto>? = ArrayList<CommentsReturnDto>()
     ){
 
 }
