2ff77dbd3d71617de2b36d53f15a109ef642c093
Egenieee <hhgg0925@naver.com>
2022-09-13 16:10:30 +0900

    [FEAT] 공지 기능 추가 - Notices 엔티티 관련 - Notices 생성 - NoticesRepository 생성

---
+++ src/main/kotlin/com/entrip/domain/entity/Notices.kt
@@ -0,0 +1,30 @@
+package com.entrip.domain.entity
+
+import com.entrip.domain.BaseTimeEntity
+import javax.persistence.*
+
+@Entity
+class Notices (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "NOTICE_ID")
+    val notice_id : Long? = null,
+
+    @Column
+    var title: String,
+    var content: String,
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "USER_ID")
+    var author: Users? = null,
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "PLANNER_ID")
+    var planners: Planners? = null
+
+) : BaseTimeEntity() {
+    fun update(title: String, content: String) : Unit {
+        this.title = title
+        this.content = content
+    }
+}
