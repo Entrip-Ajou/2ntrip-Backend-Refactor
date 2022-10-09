11a1a9dd9b0ce5fcbdd1c10735bea3da8770235c
Egenieee <hhgg0925@naver.com>
2022-10-09 12:30:41 +0900

    [REFACTOR] 공지 부분 timestamp LocalDateTime에서 String 값으로 변경

--- src/main/kotlin/com/entrip/domain/dto/Notices/NoticesReturnDto.kt
+++ src/main/kotlin/com/entrip/domain/dto/Notices/NoticesReturnDto.kt
@@ -2,19 +2,20 @@ package com.entrip.domain.dto.Notices
 
 import com.entrip.domain.entity.Notices
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class NoticesReturnDto(
     val notice_id : Long?,
     val author : String?,
     val title : String,
     val content : String,
-    val modifiedDate : LocalDateTime,
+    val modifiedDate : String,
 ) {
     constructor(notices: Notices) : this(
         notices.notice_id,
         notices.author!!.user_id,
         notices.title,
         notices.content,
-        notices.timestamp
+        notices.timestamp.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
     )
 }
