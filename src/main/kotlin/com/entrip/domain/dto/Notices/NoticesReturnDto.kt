5d846d6533812e1e34e2d30a3e7baa8fec7622cd
Egenieee <hhgg0925@naver.com>
2022-10-29 21:22:06 +0900

    [REFACTOR] (XAEX-88) NoticesReturnDto 수정
    - modifiedDate format 수정
    - nickname 추가

--- src/main/kotlin/com/entrip/domain/dto/Notices/NoticesReturnDto.kt
+++ src/main/kotlin/com/entrip/domain/dto/Notices/NoticesReturnDto.kt
@@ -1,12 +1,12 @@
 package com.entrip.domain.dto.Notices
 
 import com.entrip.domain.entity.Notices
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class NoticesReturnDto(
     val notice_id : Long?,
     val author : String?,
+    val nickname : String,
     val title : String,
     val content : String,
     val modifiedDate : String,
@@ -14,8 +14,9 @@ class NoticesReturnDto(
     constructor(notices: Notices) : this(
         notices.notice_id,
         notices.author!!.user_id,
+        notices.author!!.nickname,
         notices.title,
         notices.content,
-        notices.timestamp.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+        notices.timestamp.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
     )
 }
--- src/main/kotlin/com/entrip/domain/entity/VotesContents.kt
+++ src/main/kotlin/com/entrip/domain/entity/VotesContents.kt
@@ -11,7 +11,6 @@ import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToMany
 import javax.persistence.ManyToOne
-import javax.persistence.OneToMany
 
 @Entity
 class VotesContents (
