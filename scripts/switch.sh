47438b413f9f483f4dd01669a1da1a062566c1da
donghwan <wkazxf@gmail.com>
2022-11-14 12:49:13 +0900

    [FIX] Change switch.sh
    Change scripts, ser service_url from http to https

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -14,7 +14,7 @@ function switch_proxy() {
     echo "> Port 전환"
     # nginx와 연결한 주소 생성
     # | sudo tee ~ : 앞에서 넘긴 문장을 service-url.inc에 덮어씀
-    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "set \$service_url https://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> 엔진엑스 Reload"
     # nignx reload. restart와는 다르게 설정 값만 불러옴
