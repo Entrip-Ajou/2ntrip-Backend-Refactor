146a2a24ef104fb0e354ad68b4473090cc0d749d
donghwan <wkazxf@gmail.com>
2022-11-14 12:13:31 +0900

    [FIX] Change ProfileController from .java to .kt (XAEX-96)
    For checking profileController

--- src/main/kotlin/com/entrip/controller/WebController.kt
+++ src/main/kotlin/com/entrip/controller/WebController.kt
@@ -1,6 +1,7 @@
 package com.entrip.controller
 
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
