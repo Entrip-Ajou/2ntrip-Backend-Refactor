aa51e08990da5c088535b13b1c8cc9a78dabdfa8
donghwan <wkazxf@gmail.com>
2022-11-14 12:17:44 +0900

    [CHORE] Unblock PropertiesConfigEC2Version.kt and Block PropertiesConfigLocalVersion.kt

--- src/main/kotlin/com/entrip/config/PropertiesConfigEC2Version.kt
+++ src/main/kotlin/com/entrip/config/PropertiesConfigEC2Version.kt
@@ -1,34 +1,34 @@
-//package com.entrip.config
-//
-//import org.springframework.beans.factory.config.PropertiesFactoryBean
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.context.annotation.Primary
-//import org.springframework.core.io.PathResource
-//
-//@Primary
-//@Configuration
-//class PropertiesConfigEC2Version {
-//
-//    private val defaultEC2Path: String = "/home/ec2-user/app/step3/properties/"
-//
-//    private fun propertiesFactoryBean(path: String): PropertiesFactoryBean {
-//        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
-//        val pathResource: PathResource = PathResource(path)
-//
-//        propertiesFactoryBean.setLocation(pathResource)
-//        return propertiesFactoryBean
-//    }
-//
-//    @Bean(name = arrayOf("awsS3"))
-//    public fun propertiesFactoryBeanForS3(): PropertiesFactoryBean =
-//        propertiesFactoryBean("${defaultEC2Path}application-aws-s3.properties")
-//
-//    @Bean(name = arrayOf("redis"))
-//    public fun propertiesFactoryBeanForRedis(): PropertiesFactoryBean =
-//        propertiesFactoryBean("${defaultEC2Path}application-redis.properties")
-//
-//    @Bean(name = arrayOf("security"))
-//    public fun propertiesFactoryBeanForSecurity(): PropertiesFactoryBean =
-//        propertiesFactoryBean("${defaultEC2Path}application-security.properties")
-//}
+package com.entrip.config
+
+import org.springframework.beans.factory.config.PropertiesFactoryBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.core.io.PathResource
+
+@Primary
+@Configuration
+class PropertiesConfigEC2Version {
+
+    private val defaultEC2Path: String = "/home/ec2-user/app/step3/properties/"
+
+    private fun propertiesFactoryBean(path: String): PropertiesFactoryBean {
+        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
+        val pathResource: PathResource = PathResource(path)
+
+        propertiesFactoryBean.setLocation(pathResource)
+        return propertiesFactoryBean
+    }
+
+    @Bean(name = arrayOf("awsS3"))
+    public fun propertiesFactoryBeanForS3(): PropertiesFactoryBean =
+        propertiesFactoryBean("${defaultEC2Path}application-aws-s3.properties")
+
+    @Bean(name = arrayOf("redis"))
+    public fun propertiesFactoryBeanForRedis(): PropertiesFactoryBean =
+        propertiesFactoryBean("${defaultEC2Path}application-redis.properties")
+
+    @Bean(name = arrayOf("security"))
+    public fun propertiesFactoryBeanForSecurity(): PropertiesFactoryBean =
+        propertiesFactoryBean("${defaultEC2Path}application-security.properties")
+}
--- src/main/kotlin/com/entrip/config/PropertiesConfigLocalVersion.kt
+++ src/main/kotlin/com/entrip/config/PropertiesConfigLocalVersion.kt
@@ -1,54 +1,54 @@
-package com.entrip.config
-
-import org.springframework.beans.factory.config.PropertiesFactoryBean
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.springframework.core.io.ClassPathResource
-
-@Primary
-@Configuration
-class PropertiesConfig {
-
-    private fun propertiesFactoryBean(path: String): PropertiesFactoryBean {
-        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
-        val classPathResource: ClassPathResource = ClassPathResource(path)
-
-        propertiesFactoryBean.setLocation(classPathResource)
-        return propertiesFactoryBean
-    }
-
-    // Have to change to reusable logic
-
-//    @Bean(name = arrayOf("awsS3"))
-//    public fun propertiesFactoryBeanForS3(): PropertiesFactoryBean {
+//package com.entrip.config
+//
+//import org.springframework.beans.factory.config.PropertiesFactoryBean
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.context.annotation.Primary
+//import org.springframework.core.io.ClassPathResource
+//
+//@Primary
+//@Configuration
+//class PropertiesConfig {
+//
+//    private fun propertiesFactoryBean(path: String): PropertiesFactoryBean {
 //        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
-//        val classPathResource: ClassPathResource = ClassPathResource("application-aws-s3.properties")
+//        val classPathResource: ClassPathResource = ClassPathResource(path)
 //
 //        propertiesFactoryBean.setLocation(classPathResource)
 //        return propertiesFactoryBean
 //    }
-
-    @Bean(name = arrayOf("awsS3"))
-    public fun propertiesFactoryBeanForS3(): PropertiesFactoryBean =
-        propertiesFactoryBean("application-aws-s3.properties")
-
-    @Bean(name = arrayOf("redis"))
-    public fun propertiesFactoryBeanForRedis(): PropertiesFactoryBean {
-        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
-        val classPathResource: ClassPathResource = ClassPathResource("application-redis.properties")
-
-        propertiesFactoryBean.setLocation(classPathResource)
-        return propertiesFactoryBean
-    }
-
-    @Bean(name = arrayOf("security"))
-    public fun propertiesFactoryBeanForSecurity(): PropertiesFactoryBean {
-        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
-        val classPathResource: ClassPathResource = ClassPathResource("application-security.properties")
-
-        propertiesFactoryBean.setLocation(classPathResource)
-        return propertiesFactoryBean
-    }
-
-}
+//
+//    // Have to change to reusable logic
+//
+////    @Bean(name = arrayOf("awsS3"))
+////    public fun propertiesFactoryBeanForS3(): PropertiesFactoryBean {
+////        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
+////        val classPathResource: ClassPathResource = ClassPathResource("application-aws-s3.properties")
+////
+////        propertiesFactoryBean.setLocation(classPathResource)
+////        return propertiesFactoryBean
+////    }
+//
+//    @Bean(name = arrayOf("awsS3"))
+//    public fun propertiesFactoryBeanForS3(): PropertiesFactoryBean =
+//        propertiesFactoryBean("application-aws-s3.properties")
+//
+//    @Bean(name = arrayOf("redis"))
+//    public fun propertiesFactoryBeanForRedis(): PropertiesFactoryBean {
+//        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
+//        val classPathResource: ClassPathResource = ClassPathResource("application-redis.properties")
+//
+//        propertiesFactoryBean.setLocation(classPathResource)
+//        return propertiesFactoryBean
+//    }
+//
+//    @Bean(name = arrayOf("security"))
+//    public fun propertiesFactoryBeanForSecurity(): PropertiesFactoryBean {
+//        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
+//        val classPathResource: ClassPathResource = ClassPathResource("application-security.properties")
+//
+//        propertiesFactoryBean.setLocation(classPathResource)
+//        return propertiesFactoryBean
+//    }
+//
+//}
