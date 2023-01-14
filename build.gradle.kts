be145fac0fcf1f0b049f38d0b5485e2d1d820289
Egenieee <hhgg0925@naver.com>
2023-01-14 21:06:42 +0900

    [CHORE] (XAEX-110) set gradle.build

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.apache.commons:commons-exec:1.3")
+	implementation("org.apache.commons:commons-exec:1.3")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 }
 
