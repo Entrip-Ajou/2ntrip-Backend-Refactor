ce4868516643c91bfe9259fdd0b8c66eb11036ca
donghwan <wkazxf@gmail.com>
2023-01-31 23:52:57 +0900

    [FEAT] Running Python TravelRecommend With Apache Common Exec (XAEX-109)

--- src/main/kotlin/com/entrip/config/PropertiesConfigEC2Version.kt
+++ src/main/kotlin/com/entrip/config/PropertiesConfigEC2Version.kt
@@ -1,38 +1,38 @@
-package com.entrip.config
-
-import org.springframework.beans.factory.config.PropertiesFactoryBean
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.springframework.core.io.PathResource
-
-@Primary
-@Configuration
-class PropertiesConfigEC2Version {
-
-    private val defaultEC2Path: String = "/home/ec2-user/app/step3/properties/"
-
-    private fun propertiesFactoryBean(path: String): PropertiesFactoryBean {
-        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
-        val pathResource: PathResource = PathResource(path)
-
-        propertiesFactoryBean.setLocation(pathResource)
-        return propertiesFactoryBean
-    }
-
-    @Bean(name = arrayOf("awsS3"))
-    public fun propertiesFactoryBeanForS3(): PropertiesFactoryBean =
-        propertiesFactoryBean("${defaultEC2Path}application-aws-s3.properties")
-
-    @Bean(name = arrayOf("redis"))
-    public fun propertiesFactoryBeanForRedis(): PropertiesFactoryBean =
-        propertiesFactoryBean("${defaultEC2Path}application-redis.properties")
-
-    @Bean(name = arrayOf("security"))
-    public fun propertiesFactoryBeanForSecurity(): PropertiesFactoryBean =
-        propertiesFactoryBean("${defaultEC2Path}application-security.properties")
-
-    @Bean(name = arrayOf("mongodb"))
-    public fun propertiesFactoryBeanForMongodb(): PropertiesFactoryBean =
-        propertiesFactoryBean("${defaultEC2Path}application-mongodb.properties")
-}
+//package com.entrip.config
+//
+//import org.springframework.beans.factory.config.PropertiesFactoryBean
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.context.annotation.Primary
+//import org.springframework.core.io.PathResource
+//
+//@Primary
+//@Configuration
+//class PropertiesConfigEC2Version {
+//
+//    private val defaultEC2Path: String = "/home/ec2-user/app/step3/properties/"
+//
+//    private fun propertiesFactoryBean(path: String): PropertiesFactoryBean {
+//        val propertiesFactoryBean: PropertiesFactoryBean = PropertiesFactoryBean()
+//        val pathResource: PathResource = PathResource(path)
+//
+//        propertiesFactoryBean.setLocation(pathResource)
+//        return propertiesFactoryBean
+//    }
+//
+//    @Bean(name = arrayOf("awsS3"))
+//    public fun propertiesFactoryBeanForS3(): PropertiesFactoryBean =
+//        propertiesFactoryBean("${defaultEC2Path}application-aws-s3.properties")
+//
+//    @Bean(name = arrayOf("redis"))
+//    public fun propertiesFactoryBeanForRedis(): PropertiesFactoryBean =
+//        propertiesFactoryBean("${defaultEC2Path}application-redis.properties")
+//
+//    @Bean(name = arrayOf("security"))
+//    public fun propertiesFactoryBeanForSecurity(): PropertiesFactoryBean =
+//        propertiesFactoryBean("${defaultEC2Path}application-security.properties")
+//
+//    @Bean(name = arrayOf("mongodb"))
+//    public fun propertiesFactoryBeanForMongodb(): PropertiesFactoryBean =
+//        propertiesFactoryBean("${defaultEC2Path}application-mongodb.properties")
+//}
--- src/main/kotlin/com/entrip/controller/TravelRecommendController.kt
+++ src/main/kotlin/com/entrip/controller/TravelRecommendController.kt
@@ -1,16 +1,38 @@
 package com.entrip.controller
 
+import com.entrip.domain.RestAPIMessages
+import com.entrip.domain.dto.TravelRecommendRequestDto.TravelRecommendRequestDto
 import com.entrip.service.TravelRecommendService
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.nio.charset.Charset
 
 @RestController
 class TravelRecommendController(
     private val travelRecommendService: TravelRecommendService
 ) {
 
-    @GetMapping("/pythonTest")
-    fun exec_python() {
-        travelRecommendService.call_python()
+    private fun sendResponseHttpByJson(message: String, data: Any): ResponseEntity<RestAPIMessages> {
+        val restAPIMessages: RestAPIMessages = RestAPIMessages(
+            httpStatus = 200,
+            message = message,
+            data = data
+        )
+        val headers: HttpHeaders = HttpHeaders()
+        headers.contentType = MediaType("application", "json", Charset.forName("UTF-8"))
+        return ResponseEntity<RestAPIMessages>(restAPIMessages, headers, HttpStatus.OK)
+    }
+
+    @GetMapping("/api/v2/travelRecommend")
+    fun execPython(@RequestBody requestDto: TravelRecommendRequestDto): ResponseEntity<RestAPIMessages> {
+        return sendResponseHttpByJson(
+            "TravelRecommend!",
+            travelRecommendService.callPython(requestDto.user_id, requestDto.region, requestDto.recommendType)
+        )
     }
 }
