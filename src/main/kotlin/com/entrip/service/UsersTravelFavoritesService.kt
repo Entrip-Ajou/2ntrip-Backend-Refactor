b86cf3ece6473340c5904f5034b549707e3ea968
Egenieee <hhgg0925@naver.com>
2023-02-16 19:26:35 +0900

    [FEAT] (XAEX-108) 단일 지역 점수 저장 로직에서 list 형태로 여러 개의 지역 점수를 저장할 수 있도록 수정

--- src/main/kotlin/com/entrip/service/UsersTravelFavoritesService.kt
+++ src/main/kotlin/com/entrip/service/UsersTravelFavoritesService.kt
@@ -1,6 +1,7 @@
 package com.entrip.service
 
-import com.entrip.domain.entity.TravelFavorite
+import com.entrip.domain.dto.UsersTravelFavoriteRequestDto.EachTravelFavoriteSaveRequestDto
+import com.entrip.domain.dto.UsersTravelFavoriteRequestDto.UsersTravelFavoriteSaveRequestDto
 import com.entrip.domain.entity.UsersTravelFavorites
 import com.entrip.repository.UsersTravelFavoritesRepository
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -11,11 +12,26 @@ class UsersTravelFavoritesService(
     private final val usersTravelFavoritesRepository: UsersTravelFavoritesRepository,
     private val objectMapper: ObjectMapper
 ) {
-    fun addUsersTravelFavorite(user_id: String, travelFavorite: TravelFavorite): UsersTravelFavorites {
-        if (!usersTravelFavoritesRepository.existsById(user_id)) saveUsersTravelFavorite(user_id)
+    fun addUsersTravelFavorite(user_id: String, requestDto: UsersTravelFavoriteSaveRequestDto): UsersTravelFavorites {
+        // 유저가 추천받기 위한 usersTravelFavorite 엔티티를 가지고 않을 경우 새로 하나 만든다.
+        if (!usersTravelFavoritesRepository.existsById(user_id)) {
+            saveUsersTravelFavorite(user_id)
+        }
+
         val target = usersTravelFavoritesRepository.findById(user_id).get()
-        target.addTravelFavorite(travelFavorite)
+
+        // for문 돌려가며 list에서 값 하나씩 빼와서 travelFavorite dto 만들어서 유저에 붙인다.
+        for (index in requestDto.regions.indices) {
+            val region : String = requestDto.regions[index]
+            val score : Int = requestDto.scores[index]
+
+            val travelFavoriteSaveRequestDto : EachTravelFavoriteSaveRequestDto = EachTravelFavoriteSaveRequestDto(region, score)
+
+            target.addTravelFavorite(travelFavoriteSaveRequestDto.toEntity());
+        }
+
         usersTravelFavoritesRepository.save(target)
+
         return usersTravelFavoritesRepository.findById(user_id).get()
     }
 
