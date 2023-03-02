d2796a213068431026610f95a323139f6be7b98d
Egenieee <hhgg0925@naver.com>
2023-03-02 16:45:42 +0900

    [FEAT] (XAEX-108) 파이썬 모델에 넣기 위한 지역 추출 로직 추가

--- src/main/kotlin/com/entrip/service/TravelRecommendService.kt
+++ src/main/kotlin/com/entrip/service/TravelRecommendService.kt
@@ -1,7 +1,10 @@
 package com.entrip.service
 
-import com.entrip.domain.dto.TravelRecommend.TravelRecommendRequestDto
 import com.entrip.domain.dto.TravelRecommend.TravelRecommendResponseDto
+import com.entrip.domain.dto.UsersTravelFavorite.TravelFavoriteDtoComparator
+import com.entrip.domain.entity.TravelFavorite
+import com.entrip.domain.entity.UsersTravelFavorites
+import com.entrip.repository.UsersTravelFavoritesRepository
 import org.apache.commons.exec.CommandLine
 import org.apache.commons.exec.DefaultExecutor
 import org.apache.commons.exec.PumpStreamHandler
@@ -9,32 +12,50 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.io.ByteArrayOutputStream
+import java.util.Collections
 
 @Service
-class TravelRecommendService {
-
+class TravelRecommendService(
+    private final val usersTravelFavoritesRepository : UsersTravelFavoritesRepository
+) {
     private var logger: Logger = LoggerFactory.getLogger(TravelRecommendService::class.java)
 
     //private val localPath : String = "~~"
     private val EC2Path: String = "/home/ec2-user/app/step1/entrip-api-kotlin/src/main/resources/CollaborativeFiltering"
     private val localPath: String = "/Users/donghwan/Downloads/CollaborativeFiltering"
 
-    fun callPython(requestDto : TravelRecommendRequestDto): TravelRecommendResponseDto {
+    fun callPython(user_id : String): TravelRecommendResponseDto {
+        val region : String = getHighScoreRegion(user_id)
         val recommendRegions : MutableList<String> = ArrayList()
 
-        recommendRegions.add(getModelResult(requestDto, 1))
-        recommendRegions.add(getModelResult(requestDto, 2))
-        recommendRegions.add(getModelResult(requestDto, 3))
+        recommendRegions.add(getModelResult(user_id, region, 1))
+        recommendRegions.add(getModelResult(user_id, region, 2))
+        recommendRegions.add(getModelResult(user_id, region, 3))
 
-        logger.info("user_id is ${requestDto.user_id}")
-        logger.info("in callPython method : $recommendRegions")
-
-        return TravelRecommendResponseDto(user_id = requestDto.user_id, recommendRegions = recommendRegions)
+        return TravelRecommendResponseDto(user_id = user_id, recommendRegions = recommendRegions)
     }
 
-    fun getModelResult(requestDto: TravelRecommendRequestDto, recommendType : Int) : String {
+    // 정렬해서 가장 높은 점수를 받은 지역 하나 추출
+    private fun getHighScoreRegion(user_id: String): String {
+        val travelFavoriteSet : MutableSet<TravelFavorite> = findSetById(user_id)
+        val travelFavoriteList : MutableList<TravelFavorite> = ArrayList(travelFavoriteSet)
+
+        Collections.sort(travelFavoriteList, TravelFavoriteDtoComparator())
+
+        return travelFavoriteList[0].region
+    }
+
+    fun findSetById(user_id : String) : MutableSet<TravelFavorite> {
+        val usersTravelFavorite : UsersTravelFavorites = usersTravelFavoritesRepository.findById(user_id).orElseThrow {
+            IllegalArgumentException("Error raise at usersTravelFavoritesRepository.findById$user_id")
+        }
+
+        return usersTravelFavorite.travelFavorite_set
+    }
+
+    fun getModelResult(user_id: String, region: String, recommendType : Int) : String {
         val command: Array<String> =
-            arrayOf("python3", "$EC2Path/main.py", requestDto.user_id, requestDto.region, recommendType.toString())
+            arrayOf("python3", "$EC2Path/main.py", user_id, region, recommendType.toString())
         var result: String = ""
         try {
             result = execPython(command)
