642e829c15cb661bc95e1512f3a9132ed099e360
Egenieee <hhgg0925@naver.com>
2023-04-24 12:13:12 +0900

    [REFACTOR] PlannersController 리팩토링 (REF-24)
    - 불필요한 접근제한자, 타입제거
    - 함수형 프로그래밍을 사용해 클린코드 적용

--- src/main/kotlin/com/entrip/controller/PlannersController.kt
+++ src/main/kotlin/com/entrip/controller/PlannersController.kt
@@ -1,13 +1,9 @@
 package com.entrip.controller
 
 import com.entrip.domain.RestAPIMessages
-import com.entrip.domain.dto.Notices.NoticesReturnDto
 import com.entrip.domain.dto.Planners.PlannersReturnDto
 import com.entrip.domain.dto.Planners.PlannersSaveRequestDto
 import com.entrip.domain.dto.Planners.PlannersUpdateRequestDto
-import com.entrip.domain.dto.Plans.PlansReturnDto
-import com.entrip.domain.dto.Users.UsersResponseDto
-import com.entrip.domain.dto.Votes.VotesReturnDto
 import com.entrip.exception.NotAcceptedException
 import com.entrip.service.PlannersService
 import org.springframework.http.ResponseEntity
@@ -15,20 +11,12 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 class PlannersController(
-    final val plannersService: PlannersService
+    val plannersService: PlannersService
 ) : BaseController() {
 
-//    @PostMapping("/api/v1/planners")
-//    public fun save (@RequestBody requestDto : PlannersSaveRequestDto) : ResponseEntity<Messages> {
-//        val savedPlannerId : Long? = plannersService.save(requestDto)
-//        val responseDto = plannersService.findByPlannerId(savedPlannerId!!)
-//        val returnDto = PlannersReturnDto(responseDto)
-//        return sendResponseHttpByJson("Planner is saved well", returnDto)
-//    }
-
     @PostMapping("/api/v1/planners/{user_id}")
-    public fun save(@PathVariable user_id: String): ResponseEntity<RestAPIMessages> {
-        val requestDto: PlannersSaveRequestDto = PlannersSaveRequestDto(user_id)
+    fun save(@PathVariable user_id: String): ResponseEntity<RestAPIMessages> {
+        val requestDto = PlannersSaveRequestDto(user_id)
         val savedPlannerId: Long? = plannersService.save(requestDto)
         val responseDto = plannersService.findByPlannerId(savedPlannerId!!)
         val returnDto = PlannersReturnDto(responseDto)
@@ -36,7 +24,7 @@ class PlannersController(
     }
 
     @PutMapping("/api/v1/planners/{planner_id}")
-    public fun update(
+    fun update(
         @PathVariable planner_id: Long,
         @RequestBody requestDto: PlannersUpdateRequestDto
     ): ResponseEntity<RestAPIMessages> {
@@ -47,102 +35,95 @@ class PlannersController(
     }
 
     @GetMapping("/api/v1/planners/{planner_id}")
-    public fun findById(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> {
+    fun findById(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> {
         val responseDto = plannersService.findByPlannerId(planner_id)
         val returnDto = PlannersReturnDto(responseDto)
         return sendResponseHttpByJson("Load planner with $planner_id", returnDto)
     }
 
-    @GetMapping("api/v1/planners/{planner_id}/all")
-    public fun findAllPlansWithPlannerId(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> {
-        val plansList: MutableList<PlansReturnDto> = plannersService.findAllPlansWithPlannerId(planner_id)
-        return sendResponseHttpByJson("Load all plans with specific planner id : $planner_id", plansList)
-    }
-
-    @GetMapping("/api/v1/planners/{planner_id}/allNotices")
-    fun findAllNoticesWithPlannerId(@PathVariable planner_id: Long) : ResponseEntity<RestAPIMessages> {
-        val returnDtoList : MutableList<NoticesReturnDto> = plannersService.findAllNoticesWithPlannerId(planner_id)
-        return sendResponseHttpByJson("Get all notices with planner id : $planner_id", returnDtoList)
-    }
-
-    @GetMapping("/api/v1/planners/{planner_id}/allVotes")
-    fun findAllVotesByPlannerId(@PathVariable planner_id :Long) : ResponseEntity<RestAPIMessages> {
-        val votesList : MutableList<VotesReturnDto>  = plannersService.findAllVotesWithPlannerID(planner_id)
-
-        return sendResponseHttpByJson("Load votes with planner id : $planner_id", votesList)
-    }
-
-    @GetMapping("api/v1/planners/{planner_id}/exist")
-    public fun plannerIsExistWithId(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> {
-        val isExist: Boolean = plannersService.plannerIsExistWithId(planner_id)
-        if (!isExist) throw NotAcceptedException(false)
-        return sendResponseHttpByJson("Find if planner is exist with specific planner id : $planner_id", isExist)
-    }
-
     @DeleteMapping("/api/v1/planners/{planner_id}")
-    public fun delete(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> {
-        val deletedPlannerId: Long = plannersService.delete(planner_id)
-        return sendResponseHttpByJson("Delete planner with id : $planner_id", deletedPlannerId)
-    }
+    fun delete(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> =
+        sendResponseHttpByJson("Delete planner with id : $planner_id", plannersService.delete(planner_id))
+
+    @DeleteMapping("api/v1/planners/{planner_id}/{user_id}/delete")
+    fun deleteWithExit(@PathVariable planner_id: Long, @PathVariable user_id: String): ResponseEntity<RestAPIMessages> =
+        sendResponseHttpByJson("Delete planner ${planner_id}", plannersService.deleteWithExit(planner_id, user_id))
+
+    @DeleteMapping("api/v1/planners/{planner_id}/{user_id}/exit")
+    fun exitPlanner(@PathVariable planner_id: Long, @PathVariable user_id: String): ResponseEntity<RestAPIMessages> =
+        sendResponseHttpByJson(
+            "User ${user_id} exit planner ${planner_id}",
+            plannersService.exitPlanner(planner_id, user_id)
+        )
 
     @PutMapping("api/v1/planners/{planner_id}/{user_id}")
-    public fun addFriendToPlanner(
+    fun addFriendToPlanner(
         @PathVariable planner_id: Long,
         @PathVariable user_id: String
-    ): ResponseEntity<RestAPIMessages> {
-        return sendResponseHttpByJson(plannersService.addFriendToPlanner(planner_id, user_id), planner_id)
-    }
+    ): ResponseEntity<RestAPIMessages> =
+        sendResponseHttpByJson(plannersService.addFriendToPlanner(planner_id, user_id), planner_id)
 
     @GetMapping("api/v1/planners/{planner_id}/getAllUser")
-    public fun findAllUsersWithPlannerId(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> {
-        val usersList: MutableList<UsersResponseDto> = plannersService.findAllUsersWithPlannerId(planner_id)
-        return sendResponseHttpByJson("Load all users with planner id : $planner_id", usersList)
+    fun findAllUsersWithPlannerId(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> =
+        sendResponseHttpByJson(
+            "Load all users with planner id : $planner_id",
+            plannersService.findAllUsersWithPlannerId(planner_id)
+        )
+
+    @GetMapping("api/v1/planners/{planner_id}/all")
+    fun findAllPlansWithPlannerId(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> =
+        sendResponseHttpByJson(
+            "Load all plans with specific planner id : $planner_id",
+            plannersService.findAllPlansWithPlannerId(planner_id)
+        )
+
+    @GetMapping("api/v1/planners/{planner_id}/{date}/find")
+    fun findByPlannerIdWithDate(
+        @PathVariable planner_id: Long,
+        @PathVariable date: String
+    ): ResponseEntity<RestAPIMessages> = sendResponseHttpByJson(
+        "Get all plans with specific date $date, planner_id $planner_id",
+        plannersService.findByPlannerIdWithDate(planner_id, date)
+    )
+
+    @GetMapping("/api/v1/planners/{planner_id}/allNotices")
+    fun findAllNoticesWithPlannerId(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> =
+        sendResponseHttpByJson(
+            "Get all notices with planner id : $planner_id",
+            plannersService.findAllNoticesWithPlannerId(planner_id)
+        )
+
+    @GetMapping("/api/v1/planners/{planner_id}/allVotes")
+    fun findAllVotesByPlannerId(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> =
+        sendResponseHttpByJson(
+            "Load votes with planner id : $planner_id",
+            plannersService.findAllVotesWithPlannerID(planner_id)
+        )
+
+    @GetMapping("api/v1/planners/{planner_id}/exist")
+    fun plannerIsExistWithId(@PathVariable planner_id: Long): ResponseEntity<RestAPIMessages> {
+        val isExist: Boolean = plannersService.plannerIsExistWithId(planner_id)
+        if (!isExist) throw NotAcceptedException(false)
+        return sendResponseHttpByJson("Find if planner is exist with specific planner id : $planner_id", true)
     }
 
     @GetMapping("api/v1/planners/{planner_id}/{user_id}/exist")
-    public fun userIsExistInPlannerWithUserId(
+    fun userIsExistInPlannerWithUserId(
         @PathVariable planner_id: Long,
         @PathVariable user_id: String
     ): ResponseEntity<RestAPIMessages> {
         val isExist: Boolean = plannersService.userIsExistInPlannerWithUserId(planner_id, user_id)
         if (!isExist) throw NotAcceptedException(false)
-        return sendResponseHttpByJson("Check if user : $user_id is exist at planner : $planner_id", isExist)
+        return sendResponseHttpByJson("Check if user : $user_id is exist at planner : $planner_id", true)
     }
 
     @GetMapping("api/v1/planners/{planner_id}/{nickname}/exist/nickname")
-    public fun userIsExistInPlannerWithNickname(
+    fun userIsExistInPlannerWithNickname(
         @PathVariable planner_id: Long,
         @PathVariable nickname: String
     ): ResponseEntity<RestAPIMessages> {
         val isExist: Boolean = plannersService.userIsExistInPlannerWithNickname(planner_id, nickname)
         if (!isExist) throw NotAcceptedException(false)
-        return sendResponseHttpByJson("Check if user : $nickname is exist at planner : $planner_id", isExist)
-    }
-
-    @DeleteMapping("api/v1/planners/{planner_id}/{user_id}/delete")
-    public fun deleteWithExit(
-        @PathVariable planner_id: Long,
-        @PathVariable user_id: String
-    ): ResponseEntity<RestAPIMessages> {
-        val long = plannersService.deleteWithExit(planner_id, user_id)
-        return sendResponseHttpByJson("Delete planner ${planner_id}", planner_id)
-    }
-
-    @DeleteMapping("api/v1/planners/{planner_id}/{user_id}/exit")
-    public fun exitPlanner(
-        @PathVariable planner_id: Long,
-        @PathVariable user_id: String
-    ): ResponseEntity<RestAPIMessages> {
-        val response: Boolean = plannersService.exitPlanner(planner_id, user_id)
-        return sendResponseHttpByJson("User ${user_id} exit planner ${planner_id}", response)
-    }
-
-    @GetMapping("api/v1/planners/{planner_id}/{date}/find")
-    public fun findByPlannerIdWithDate(
-        @PathVariable planner_id: Long,
-        @PathVariable date: String
-    ): ResponseEntity<RestAPIMessages> {
-        val response: MutableList<PlansReturnDto> = plannersService.findByPlannerIdWithDate(planner_id, date)
-        return sendResponseHttpByJson("Get all plans with specific date $date, planner_id $planner_id", response)
+        return sendResponseHttpByJson("Check if user : $nickname is exist at planner : $planner_id", true)
     }
 }
