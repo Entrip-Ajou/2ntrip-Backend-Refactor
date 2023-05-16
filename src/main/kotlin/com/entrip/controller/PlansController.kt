f8e1bc111f439e56248ba16546d02e9c9b761464
Egenieee <hhgg0925@naver.com>
2023-05-16 17:30:48 +0900

    [REFACTOR] PlansController 리팩토링 (REF-33)
    - BaseController 사용
    - 불필요한 접근제어자 제거

--- src/main/kotlin/com/entrip/controller/PlansController.kt
+++ src/main/kotlin/com/entrip/controller/PlansController.kt
@@ -6,57 +6,42 @@ import com.entrip.domain.dto.Plans.PlansReturnDto
 import com.entrip.domain.dto.Plans.PlansSaveRequestDto
 import com.entrip.domain.dto.Plans.PlansUpdateRequestDto
 import com.entrip.service.PlansService
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.nio.charset.Charset
 
 @RestController
 class PlansController(
-    final val plansService: PlansService
-) {
-
-    private fun sendResponseHttpByJson(message: String, data: Any): ResponseEntity<RestAPIMessages> {
-        val restAPIMessages: RestAPIMessages = RestAPIMessages(
-            httpStatus = 200,
-            message = message,
-            data = data
-        )
-        val headers: HttpHeaders = HttpHeaders()
-        headers.contentType = MediaType("application", "json", Charset.forName("UTF-8"))
-        return ResponseEntity<RestAPIMessages>(restAPIMessages, headers, HttpStatus.OK)
-    }
+    val plansService: PlansService
+) : BaseController() {
 
     @PostMapping("/api/v1/plans")
-    public fun save(@RequestBody requestDto: PlansSaveRequestDto): ResponseEntity<com.entrip.domain.RestAPIMessages> {
+    fun save(@RequestBody requestDto: PlansSaveRequestDto): ResponseEntity<RestAPIMessages> {
         val savedPlanId: Long = plansService.save(requestDto)!!
-        val responseDto: PlansResponseDto = plansService.findById(savedPlanId!!)
-        val returnDto: PlansReturnDto = PlansReturnDto(responseDto)
+        val responseDto: PlansResponseDto = plansService.findById(savedPlanId)
+        val returnDto = PlansReturnDto(responseDto)
         return sendResponseHttpByJson("Plan is saved well", returnDto)
     }
 
     @PutMapping("/api/v1/plans/{plan_id}")
-    public fun update(
+    fun update(
         @PathVariable plan_id: Long,
         @RequestBody requestDto: PlansUpdateRequestDto
     ): ResponseEntity<RestAPIMessages> {
-        val updatedPlannerId = plansService.update(plan_id, requestDto)
+        plansService.update(plan_id, requestDto)
         val responseDto = plansService.findById(plan_id)
         val returnDto = PlansReturnDto(responseDto)
         return sendResponseHttpByJson("Plan is updated well", returnDto)
     }
 
     @GetMapping("/api/v1/plans/{plan_id}")
-    public fun findById(@PathVariable plan_id: Long): ResponseEntity<RestAPIMessages> {
+    fun findById(@PathVariable plan_id: Long): ResponseEntity<RestAPIMessages> {
         val responseDto = plansService.findById(plan_id)
         val returnDto = PlansReturnDto(responseDto)
         return sendResponseHttpByJson("Load plan with id : $plan_id", returnDto)
     }
 
     @DeleteMapping("/api/v1/plans/{plan_id}")
-    public fun delete(@PathVariable plan_id: Long): ResponseEntity<RestAPIMessages> {
+    fun delete(@PathVariable plan_id: Long): ResponseEntity<RestAPIMessages> {
         val deletedPlanId = plansService.delete(plan_id)
         return sendResponseHttpByJson("Delete plan with id : $plan_id", deletedPlanId)
     }
